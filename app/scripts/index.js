d3ffa70f05b07a121929fe4e5138745602e70a80
Alyssa Sinicropi <asinicropi@aim.com>
2016-10-18 09:02:26 -0400

    finished

---
+++ app/scripts/index.js
@@ -0,0 +1,21 @@
+var $ = require('jquery');
+var Like = require('./models/like').Like;
+
+var likeButton = $('.like-button');
+var numValue = document.getElementById("num-value");
+
+var like = new Like();
+
+numValue.textContent = ('0 Likes');
+
+likeButton.on('click', function(event){
+  event.preventDefault();
+
+  like.likeValue();
+
+    if(like.get('likes') !== 1) {
+      numValue.textContent = (like.get('likes') + " Likes");
+    }else {
+      numValue.textContent = (like.get('likes') + " Like");
+    };
+});
